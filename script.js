c07264730dd178a15db829693f25a6cc9f4ae47b
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-12 13:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	let qV = parseInt(c/25) ;
+	c = c - qV*25;
+	let dV = parseInt(c/10) ;
+	c = c-dV*10;
+	let nV = parseInt(c/5) ;
+	c = c-nV*5;
+	let pV = parseInt(c/1) ;
+
+	let user = {
+		q : qV,
+		d : dV,
+		n : nV,
+		p : pV
+	}
+	return user;
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(c))); 
